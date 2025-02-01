fab9efdb9c69ce73bf3981a685f9aaefa37f75ab
Alex on silver bike <147690249+alexandrpodobryaev@users.noreply.github.com>
2025-02-02 02:35:17 +0400

    tests folder for lesson 1. albums.py

--- lesson1/albums.py
+++ lesson1/albums.py
@@ -1,35 +1,40 @@
 """
 Создай класс Album у которого есть поля
-
 - Исполнитель (artist) - строка
 - Название (title) - строка
-- Треки (tracks) - это **список**
-
+- Треки (tracks) - это **список**
 **Создай два экземпляра album_1 и album_2**
-
 Исполнитель: Queen
-
 Название: Killer Queen
-
 Треки: Brighton rock, Killer Queen, Tenement Funster
-
 Исполнитель: Metallica
-
 Название: Black Album
-
 Треки: Enter Sandman, Sad But True, Holier Than Thou
 """
 
 
-class ...:
+class Album:
+    artist: str
+    title: str
+    tracks: list
 
-    def ...:
-    ...
+    def __init__(self, artist, title, tracks):
+        self.artist = artist
+        self.title = title
+        self.tracks = tracks
 
 
-album_1 = Album(...)
+album_1 = Album('Queen', 'Killer Queen', [
+    'Brighton rock',
+    'Killer Queen',
+    'Tenement Funster'
+])
 
-album_2 = ...
+album_2 = Album('Metallica', 'Black Album', [
+    'Enter Sandman',
+    'Sad But True',
+    'Holier Than Thou'
+])
 
 
 # код для проверки 
